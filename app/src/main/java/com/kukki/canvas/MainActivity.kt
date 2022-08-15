71c06200dd826acdbdc4414180d3ecac78c1c343
shraddha <shraddhatongya97@gmail.com>
2022-08-15 13:46:34 +0530

    ui update

--- app/src/main/java/com/kukki/canvas/MainActivity.kt
+++ app/src/main/java/com/kukki/canvas/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.kukki.canvas.ui.screen.PaintApp
 import com.kukki.canvas.ui.theme.CanvasTheme
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/kukki/canvas/ui/screen/PaintApp.kt
+++ app/src/main/java/com/kukki/canvas/ui/screen/PaintApp.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
@@ -19,13 +18,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kukki.canvas.ui.component.horizontalGradientBackground
@@ -51,10 +51,14 @@ fun PaintApp() {
         val paths = rememberPathStateList()
         Scaffold(
             topBar = {
-                PaintAppBar(
-                    bgColor = bgColor,
-                    onDelete = {
-                        paths.value = mutableListOf()
+                TopAppBar(
+                    modifier = Modifier.fillMaxWidth(),
+                    backgroundColor = bgColor,
+                    title = {
+                        Text(modifier = Modifier.fillMaxWidth(), text = "Canvas", textAlign = TextAlign.Center)
+                    },
+                    actions = {
+
                     }
                 )
             },
@@ -66,9 +70,9 @@ fun PaintApp() {
                             onColorSelected = { color ->
                                 drawColor.value = color
                             })
-                    }
 
-                    UsedColorSelectionRowView(usedColors, drawColor)
+                        UsedColorSelectionRowView(usedColors, drawColor)
+                    }
 
                     paths.value.add(PathState(path = Path(), color = drawColor.value, stroke = drawBrush.value))
 
@@ -80,67 +84,46 @@ fun PaintApp() {
 
             },
             bottomBar = {
-                BottomAppBar(
-                    backgroundColor = bgColor
-                ) {
+                BottomAppBar(backgroundColor = bgColor) {
                     val modifier = Modifier
                         .weight(0.1f)
 
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Icon(
-                            modifier = modifier
-                                .clickable {
-                                    showBrushes.value = !showBrushes.value
-                                },
-                            imageVector = Icons.Default.Brush,
-                            contentDescription = "stroke selector",
-                            tint = drawColor.value
-                        )
-
-                        Divider(
-                            modifier = Modifier.width(1.dp)
-                        )
-
-                        Icon(
-                            modifier = modifier
-                                .clickable {
-                                    showColorPicker.value = !showColorPicker.value
-                                },
-                            imageVector = Icons.Default.Colorize,
-                            contentDescription = "color picker",
-                            tint = drawColor.value
-                        )
+                    Row(verticalAlignment = Alignment.CenterVertically) {
 
 
-                        Divider(
-                            modifier = Modifier.width(1.dp)
-                        )
+                        BottomBarButtonView(
+                            modifier = modifier,
+                            icon = Icons.Default.Brush,
+                            label = "Stroke width",
+                            onClick = {
+                                showBrushes.value = !showBrushes.value
+                            })
 
-                        Icon(
-                            modifier = modifier
-                                .clickable {
-                                    showColorPicker.value = !showColorPicker.value
-                                },
-                            imageVector = Icons.Default.Save,
-                            contentDescription = "save drawing",
-                            tint = drawColor.value
-                        )
 
-                        Divider(
-                            modifier = Modifier.width(1.dp)
-                        )
+                        BottomBarButtonView(
+                            modifier = modifier,
+                            icon = Icons.Default.Colorize,
+                            label = "Pick Color",
+                            onClick = {
+                                showColorPicker.value = !showColorPicker.value
+                            })
 
-                        Icon(
-                            modifier = modifier
-                                .clickable {
 
-                                },
-                            imageVector = Icons.Default.SavedSearch,
-                            contentDescription = "used colors",
-                            tint = drawColor.value
-                        )
+                        BottomBarButtonView(
+                            modifier = modifier,
+                            icon = Icons.Default.Save,
+                            label = "Save",
+                            onClick = {
+
+                            })
+
+                        BottomBarButtonView(
+                            modifier = modifier,
+                            icon = Icons.Default.Delete,
+                            label = "Delete",
+                            onClick = {
+                                paths.value = mutableListOf()
+                            })
                     }
                 }
             }
@@ -149,10 +132,32 @@ fun PaintApp() {
 }
 
 @Composable
-private fun UsedColorSelectionRowView(
-    usedColors: MutableState<MutableSet<Color>>,
-    drawColor: MutableState<Color>
-) {
+private fun BottomBarButtonView(modifier: Modifier, icon: ImageVector, label: String, onClick: () -> Unit) {
+    IconButton(
+        modifier = modifier,
+        onClick = {
+            onClick()
+        },
+        content = {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Icon(
+                    imageVector = icon,
+                    contentDescription = label,
+                    tint = Color.White,
+                    modifier = Modifier.padding(bottom = 2.dp)
+                )
+
+                Text(
+                    modifier = Modifier.padding(top = 2.dp),
+                    text = label,
+                    color = Color.White
+                )
+            }
+        })
+}
+
+@Composable
+private fun UsedColorSelectionRowView(usedColors: MutableState<MutableSet<Color>>, drawColor: MutableState<Color>) {
     Row(
         modifier = Modifier
             .horizontalGradientBackground(listOf(graySurface, Color.Black))
@@ -174,24 +179,6 @@ private fun UsedColorSelectionRowView(
         }
     }
 }
-
-
-@Composable
-fun PaintAppBar(bgColor: Color, onDelete: () -> Unit) {
-    TopAppBar(
-        backgroundColor = bgColor,
-        title = { Text("Canvas") },
-        actions = {
-            IconButton(onClick = onDelete, content = {
-                Icon(
-                    imageVector = Icons.Default.Delete,
-                    contentDescription = null
-                )
-            })
-        }
-    )
-}
-
 
 @Composable
 fun DrawingCanvas(drawColor: MutableState<Color>, drawBrush: MutableState<Float>, usedColors: MutableState<MutableSet<Color>>, paths: List<PathState>) {
@@ -266,16 +253,6 @@ fun DrawingTools(showBrushes: MutableState<Boolean>, drawBrush: MutableState<Flo
             }
         }
     }
-}
-
-@Composable
-fun ExtendedFloatingActionButtonDemo() {
-    ExtendedFloatingActionButton(
-        icon = { Icon(Icons.Filled.Favorite, "") },
-        text = { Text("FloatingActionButton") },
-        onClick = { /*do something*/ },
-        elevation = FloatingActionButtonDefaults.elevation(8.dp)
-    )
 }
 
 @Composable
